5484eb7118b2be71c6b5f6436cb09cefbcefd3ac
swallow-enginer <tabletennissyuhei@gmail.com>
2021-01-04 12:16:04 +0900

    コールバック関数の戻し

--- pages/api/callback.jsx
+++ pages/api/callback.jsx
@@ -1,39 +1,28 @@
 import auth0 from '~//src/lib/auth0/auth0';
-import AppConst from '~/src/lib/appConst';
+// import AppConst from '~/src/lib/appConst';
 import User from "~/src/model/userModel";
 
 export default async function callback(req, res) {
   try {
-    // await auth0.handleCallback(req, res, {
-    //   onUserLoaded: async (req, res, session, state) => {
-    //     //サブからユーザー情報を取得
-    //     let user_info = await User.findOne({where: { sub_id: session.user.sub}});
+    await auth0.handleCallback(req, res, {
+      onUserLoaded: async (req, res, session, state) => {
+        //サブからユーザー情報を取得
+        const [user_info] = await User.findOrCreate({
+          where: { sub_id: session.user.sub},
+          defaults: { sub_id: session.user.sub},
+        });
         
-    //     //ユーザーが登録されていなければ登録
-    //     if (!user_info) {
-    //       user_info = await insertUser(session.user.sub)
-    //     }
-        
-    //     return {
-    //       ...session,
-          
-    //       //引数のセッション情報のユーザー情報を上書き
-    //       user: {
-    //         ...user_info,
-    //       }
-    //     };
-    //   }
-    // });
-    await auth0.handleCallback(req, res, {redirectTo: AppConst.URL.HOME})
+        return {
+          //引数のセッション情報のユーザー情報を上書き
+          user: {
+            ...user_info,
+          }
+        };
+      }
+    });
+    // await auth0.handleCallback(req, res, {redirectTo: AppConst.URL.HOME})
   } catch (error) {
     console.error(error);
     res.status(error.status || 500).end(error.message);
   }
-}
-//ユーザー情報の登録
-const insertUser = async (sub_id) => {
-  const data = await User.create({
-    sub_id :sub_id
-  });
-  return await data.dataValues;
 }
